0f132d455070676a9f6d8e6977796c45ec33a4bc
kch <kch3338@naver.com>
2017-07-14 16:29:17 +0900

    update sub-tab

--- src/js/sub-tab.js
+++ src/js/sub-tab.js
@@ -1,4 +1,5 @@
 $('.sub-menu > li').on('click', function () {
+
     if ($(this).hasClass('active')) {
         return;
     }
@@ -7,9 +8,17 @@ $('.sub-menu > li').on('click', function () {
     var subNavBtns = $(this).parent('.sub-menu').find('li');
     subNavBtns.removeClass('active');
     $(subNavBtns[subNavIndex]).addClass('active');
-/*
+
+    /*
     var subNavContents = $(this).parents('.sub-contents').find('section');
     subNavContents.removeClass('active');
     $(subNavContents[subNavIndex]).addClass('active');
     */
+
+    var toonId = $(this).attr('toon-id');
+
+    $('.sub-contents-toon').hide();
+    $('.' + toonId).show();
 });
+
+
