683d7bf03235660d19eaa6db52a15b9019a6757b
kch <kch3338@naver.com>
2017-07-20 19:00:16 +0900

    update tag collection-list

--- src/js/tag.js
+++ src/js/tag.js
@@ -1,4 +1,35 @@
 require('bootstrap');
-require('../less/tag.less')
+require('../less/tag.less');
 
 var common = require('./common');
+
+$('.age-nav > li').on('click', function () {
+    if ($(this).hasClass('active')) {
+        return;
+    }
+    var ageIndex = $(this).index();
+
+    var ageBtns = $(this).parent('.age-nav').find('li');
+    ageBtns.removeClass('active');
+    $(ageBtns[ageIndex]).addClass('active');
+
+    var ageId = $(this).attr('age-Id');
+    $('.line').hide();
+    $('.' + ageId).show();
+
+});
+
+$('.gender-nav > li').on('click', function () {
+    if ($(this).hasClass('active')) {
+        return;
+    }
+    var genderIndex = $(this).index();
+
+    var genderBtns = $(this).parent('.gender-nav').find('li');
+    genderBtns.removeClass('active');
+    $(genderBtns[genderIndex]).addClass('active');
+
+    var genderId = $(this).attr('gender-Id');
+    $('.fa-check').hide();
+    $('.' + genderId).show();
+});
